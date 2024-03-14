5a08baf3cb1135743ab1492be4eaa379620482ec
Iñigo <71695682+inigodedios@users.noreply.github.com>
2024-03-14 19:45:04 +0100

    Add redirection after log in

--- src/login.js
+++ src/login.js
@@ -1,40 +1,55 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function Login({ onLoginSuccess }) {
+function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate(); // Hook para navegar
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const response = await fetch('http://127.0.0.1:5000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        });
-        if (response.ok) {
-            const data = await response.json();
-            onLoginSuccess(data.user_id);
-        } else {
-            alert("Invalid login");
-        }
+      const response = await fetch('http://127.0.0.1:5000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      
+      if (response.ok) {
+        const data = await response.json();
+        // Aquí es donde navegas a /overview después de un inicio de sesión exitoso
+        navigate('/overview');
+      } else {
+        alert("Invalid login");
+      }
     } catch (error) {
-        console.error('Error during login:', error);
+      console.error('Error during login:', error);
     }
-};
-
-
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+        <input 
+          type="text" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} 
+          placeholder="Username" 
+          required 
+        />
+        <input 
+          type="password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          placeholder="Password" 
+          required 
+        />
         <button type="submit">Login</button>
       </form>
     </div>
   );
 }
+
 export default Login;
